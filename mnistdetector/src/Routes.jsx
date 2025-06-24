e425285e56ae4f72f6016851657837aefb9f3db8
Noah Abbott John Collins <najcollins@mun.ca>
2025-06-24 16:02:23 -0300

    Small fix

--- mnistdetector/src/Routes.jsx
+++ mnistdetector/src/Routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Detector from './pages/Detector';
-import About from './pages/About';
+
 
 function AppRoutes() {
     return (
